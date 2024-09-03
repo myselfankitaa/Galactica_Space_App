2a9f64ca4c18dfd5925f71b88fb40c0d6033f8e8
myselfankitaa <ankitaa.mishra@gmail.com>
2024-09-03 11:33:26 +0200

    Deleted comment, Applied the heading convention, remove heading element from number and wrap the number in p element

--- app/about_us/our_values/page.js
+++ app/about_us/our_values/page.js
@@ -1,64 +1,67 @@
 import styles from "./page.module.css";
 
 const OurValues = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Values" section
-  // Use the descriptions provided in /app/about_us/README.md
-  // Some inspiration ideas found in /data/inspiration_about_us
   return (
     <>
-      <div className={styles.ourValues}>
-        <p>OUR VALUES</p>
-        <div className={styles.container}>
-          <section className={styles.value}>
-            <h2>01</h2>
-            <h4>Exploration:</h4>
+      <h2>OUR VALUES</h2>
+      <div className={styles.container}>
+        <section className={styles.value}>
+          <p className={styles.valueNumber}>01</p>
+          <div className={styles.valueRest}>
+            <h3>Exploration:</h3>
             <p>
               We are driven by a deep-seated desire to explore the unknown. We
               believe that the pursuit of discovery is at the heart of human
               nature, and we are committed to pushing the boundaries of what is
               possible.
             </p>
+          </div>
 
-            <hr />
-          </section>
-          <section className={styles.value}>
-            <h2>02</h2>
-            <h4>Innovation:</h4>
+          <hr />
+        </section>
+        <section className={styles.value}>
+          <p className={styles.valueNumber}>02</p>
+          <div className={styles.valueRest}>
+            <h3>Innovation:</h3>
             <p>
               At Galactica, we prioritize cutting-edge technology and
               innovation. We are constantly evolving our spacecraft, safety
               protocols, and services to ensure that our travelers experience
               the most advanced and secure space journeys available.
             </p>
+          </div>
 
-            <hr />
-          </section>
-          <section className={styles.value}>
-            <h2>03</h2>
-            <h4>Sustainability:</h4>
+          <hr />
+        </section>
+
+        <section className={styles.value}>
+          <p className={styles.valueNumber}>03</p>
+          <div className={styles.valueRest}>
+            <h3>Sustainability:</h3>
             <p>
               We are committed to making space exploration sustainable for
               future generations. Our space missions are designed to minimize
               environmental impact, both on Earth and in space, and to foster a
               spirit of responsibility towards our universe.
             </p>
+          </div>
 
-            <hr />
-          </section>
-          <section className={styles.value}>
-            <h2>04</h2>
-            <h4>Community:</h4>
+          <hr />
+        </section>
+        <section className={styles.value}>
+          <p className={styles.valueNumber}>04</p>
+          <div className={styles.valueRest}>
+            <h3>Community:</h3>
             <p>
               Community: We believe in the power of collective exploration. Our
               journeys are not just about reaching new destinations; they are
               about building a community of space enthusiasts who share a
               passion for the stars.
             </p>
+          </div>
 
-            <hr />
-          </section>
-        </div>
+          <hr />
+        </section>
       </div>
     </>
   );
